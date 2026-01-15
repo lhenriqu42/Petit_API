57252705da10d8719e38ab77504fec7b3f7a2d0d
lhenriqu42 <luizhapsouza@gmail.com>
2026-01-15 14:47:45 -0300

    feat: enhance updateById functionality with improved validation and error handling

--- src/Modules/Product/controllers/UpdateById.ts
+++ src/Modules/Product/controllers/UpdateById.ts
@@ -4,22 +4,29 @@ import * as yup from 'yup';
 
 import { ProductProvider } from '../providers';
 import { validation } from '../../../server/shared/middleware';
-import { IProduct } from '../../../server/database/models';
+import AppError from '../../../server/shared/Errors';
+import { IUpdateBody } from '../providers/UpdateById';
 
 interface IParamProps {
     id?: number,
 }
 
-interface IBodyProps extends Omit<IProduct, 'id' | 'created_at' | 'updated_at' | 'code'> { }
+interface IBodyProps extends IUpdateBody { }
 
 const paramsValidation: yup.Schema<IParamProps> = yup.object().shape({
     id: yup.number().integer().required().moreThan(0),
 });
 
 const bodyValidation: yup.Schema<IBodyProps> = yup.object().shape({
-    name: yup.string().required().min(3).max(50),
-    sector: yup.number().required().min(1).max(4),
-    price: yup.number().required().min(0),
+    prod: yup.object().shape({
+        name: yup.string().min(3).max(255).required(),
+        sector: yup.number().integer().moreThan(0).required(),
+        price: yup.number().min(0).required(),
+    }),
+    prod_costs: yup.object().shape({
+        avg_cost: yup.number().min(0).required(),
+        last_cost: yup.number().min(0).required(),
+    }),
 });
 
 export const updateByIdValidation = validation({
@@ -28,21 +35,15 @@ export const updateByIdValidation = validation({
 });
 
 export const updateById = async (req: Request<IParamProps, {}, IBodyProps>, res: Response) => {
-    if (!req.params.id) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
+    try {
+        await ProductProvider.updateById(req.params.id!, req.body);
+        return res.status(StatusCodes.NO_CONTENT).send();
+    } catch (error) {
+        const appError = error as AppError;
+        return res.status(appError.statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR).json({
             errors: {
-                default: '"id" not found'
+                default: appError.message
             }
         });
     }
-    const result = await ProductProvider.updateById(req.params.id, req.body);
-    if (result instanceof Error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-            errors: {
-                default: result.message
-            }
-        });
-    }
-
-    return res.status(StatusCodes.NO_CONTENT).json(result);
 };
